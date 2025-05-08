f46ea9a54cd976964fa46b58ab6593d53592ed08
dev <dev@git.com>
2025-05-08 20:33:11 +0500

    fixing

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// const supabaseUrl = process.env.SUPABASEURL
-// const supabaseKey = process.env.SUPABASEKEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_AUTHOR_KEY
 
-// export const supabase = createClient(supabaseUrl, supabaseKey)
-export const supabase = createClient('https://atepfjyykoqhkyzpnziv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZXBmanl5a29xaGt5enBueml2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NjUyNDE3OSwiZXhwIjoyMDYyMTAwMTc5fQ.W4eX9Tl_ym-3hPbK6rviWamwLVC7T397O0JWHxuh5b0')
+export const supabase = createClient(supabaseUrl, supabaseKey)
